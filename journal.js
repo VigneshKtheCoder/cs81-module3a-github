721e631546587808b720911d1c9449b8ab14cc23
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-02 22:38:05 -0700

    Check if number is even or odd function()

--- journal.js
+++ journal.js
@@ -9,5 +9,16 @@ function add(a,b) {
 }
 console.log(`2+3 =`, add(2,3));
 
+//Check if a number is even
+function iseven(num) {
+  return num % 2 == 0;
+}
+console.log(`is 6 even?`, iseven(4));
+
+//Check if a number is odd
+function isodd(odd) {
+  return num % 2 != 0;
+}
+console.log(`is 5 odd?`, isodd(4));
 
 
